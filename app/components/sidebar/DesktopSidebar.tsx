67a6c4aa5679ee6b9fc2cc02d318fba5014ff14f
siyamulislam <siyamulislam1@gmail.com>
2023-05-17 15:15:42 +0600

    user avatar upto dynami active user list

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -4,7 +4,7 @@ import DesktopItem from "./DesktopItem";
 import useRoutes from "@/app/hooks/useRoutes";
 // import SettingsModal from "./SettingsModal";
 import { useState } from "react";
-// import Avatar from "../Avatar";
+import Avatar from "../Avatar";
 import { User } from "@prisma/client";
 
 interface DesktopSidebarProps {
@@ -56,8 +56,8 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
           <div 
             onClick={() => setIsOpen(true)} 
             className="cursor-pointer hover:opacity-75 transition"
-          >
-            {/* <Avatar user={currentUser} /> */}
+          > 
+            <Avatar user={currentUser} />
           </div>
         </nav>
       </div>
